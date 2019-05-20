2d42e8b0a721e770343d161d69fe4d795e015359
Henry Mesias <henry.mesias@threespot.com>
2019-05-19 21:44:11 -0400

    added button styles

--- scripts.js
+++ scripts.js
@@ -11,17 +11,33 @@ item.forEach(elem => {
   elem.addEventListener('click', function() {
     const clicked = this.id;
     if (clicked == 'choice-1') {
+      //add/remove classes on images
+      itemOne.classList.add('active');
+      itemTwo.classList.remove('active');
+      itemThree.classList.remove('active');
+
+      //add/remove classes on images
       imageOne.classList.add('active');
       imageTwo.classList.remove('active');
       imageThree.classList.remove('active');
     }
     if (clicked == 'choice-2') {
+      itemTwo.classList.add('active');
+      itemOne.classList.remove('active');
+      itemThree.classList.remove('active');
+
+      //add/remove classes on images
       imageTwo.classList.add('active');
       imageOne.classList.remove('active');
       imageThree.classList.remove('active');
     }
-    console.log(imageThree);
     if (clicked == 'choice-3') {
+      console.log('clicked three');
+      itemThree.classList.add('active');
+      itemOne.classList.remove('active');
+      itemTwo.classList.remove('active');
+
+      //add/remove classes on images
       imageThree.classList.add('active');
       imageOne.classList.remove('active');
       imageTwo.classList.remove('active');
